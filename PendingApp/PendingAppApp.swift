a77d9aba9de217cfe14c8b94ca419d636210e129
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2021-12-05 16:57:36 -0600

    Initial Commit

---
+++ PendingApp/PendingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PendingAppApp.swift
+//  PendingApp
+//
+//  Created by Kevin Luciano on 05/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PendingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
