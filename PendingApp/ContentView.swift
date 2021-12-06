228185652a3ac6934535ac452aaf54e116e6f5d2
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2021-12-05 20:43:16 -0600

    First commit

--- PendingApp/ContentView.swift
+++ PendingApp/ContentView.swift
@@ -5,13 +5,64 @@
 //  Created by Kevin Luciano on 05/12/21.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var newTodo = ""
+    @State private var allTodos: [TodoItem] = []
+    private let todosKey = "todosKey"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack{
+                HStack{
+                    TextField("Agregar pendiente...", text: $newTodo)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Button(action: {
+                        guard !self.newTodo.isEmpty else {return}
+                        self.allTodos.append(TodoItem(todo: self.newTodo))
+                        self.newTodo = ""
+                        self.saveTodos()
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                    .padding(.leading, 3)
+                } .padding()
+                List{
+                    ForEach(allTodos) {todoItem in
+                        Text(todoItem.todo)
+                    }.onDelete(perform: deleteTodos)
+                }
+            }
+            .navigationBarTitle("Pendientes")
+        } .onAppear(perform: loadTodos)
     }
+    
+    private func deleteTodos(at offsets:IndexSet){
+        self.allTodos.remove(atOffsets: offsets)
+        saveTodos()
+    }
+    
+    private func loadTodos(){
+        if let todosData = UserDefaults.standard.value(forKey: todosKey) as? Data {
+            if let todosList = try? PropertyListDecoder().decode(Array<TodoItem>.self, from: todosData) {
+                self.allTodos = todosList
+            }
+        }
+    }
+    
+    private func saveTodos(){
+    UserDefaults.standard.set( try? PropertyListEncoder().encode(self.allTodos), forKey: todosKey)
+    }
+}
+
+struct TodoItem: Codable, Identifiable{
+    let id = UUID()
+    let todo: String
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
